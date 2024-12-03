b9e1d55f1f216d874eb53fdb9a935771f40ced8f
Anderboi <Upolovnew@gmail.com>
2024-12-03 19:35:49 +0300

    feat: styledDialog component

---
+++ components/ui/styled-dialog.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "./dialog";
+import { X } from "lucide-react";
+
+export function StyledDialog({
+  title,
+  children,
+}: {
+  title: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <DialogContent className="h-full max-h-[90vh] overflow-y-scroll rounded-xl no-scrollbar sm:max-w-[520px]">
+      <DialogHeader className="sticky top-0 z-40 flex h-fit flex-row items-center justify-between rounded-lg bg-secondary px-4">
+        <DialogTitle>{title}</DialogTitle>
+        <DialogDescription className="hidden"></DialogDescription>
+        <DialogClose className="!mt-0 h-full !space-y-0">
+          <X className="h-6 w-6" />
+        </DialogClose>
+      </DialogHeader>
+      <section className="h-[calc(100%-56px)] pt-4">{children}</section>
+    </DialogContent>
+  );
+}
+
+export function StyledDialogFooter({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="sticky bottom-0 w-full rounded-lg bg-white p-4 shadow-card shadow-neutral-300">
+      {children}
+    </div>
+  );
+}
